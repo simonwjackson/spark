81a917a07a6d64817478674889c82147c8324dcc
Simon W. Jackson <simon@simonwjackson.com>
2021-01-18 16:53:09 -0600

    fix kill or close script

--- roles/x/files/bin/kill-or-close.sh
+++ roles/x/files/bin/kill-or-close.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 if [[ $(xdotool getwindowfocus getwindowname) =~ " — Mozilla Firefox" ]]; then
-  sleep .1 && xdotool key ctrl+w
+  sleep .1 && xdotool keyup "w" key --clearmodifiers "ctrl+w"
+elif [[ $(xdotool getwindowfocus getwindowname) =~ "tmux" ]]; then
+  sleep .01 && xdotool keyup "x" key --clearmodifiers "alt+x"
 else
   bspc node -c
 fi
